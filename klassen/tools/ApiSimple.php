d8f0b3d71678c97cd733ce21e188e69d2d119fbc
Maytrok <bluna.el@gmx.net>
2019-10-03 14:20:06 +0200

    Api Simple Required Params check

--- klassen/tools/ApiSimple.php
+++ klassen/tools/ApiSimple.php
@@ -13,12 +13,15 @@ class APISimple
     private $proxyGetParams = [];
 
     private $_get, $_post, $_patch, $_delete;
+    protected $requiredParams = [];
 
 
     protected function get()
     {
         if (!$this->_get)
             throw new Exception("Request Method not Allowed", 405);
+
+        $this->checkRequiredParams("get", $_GET);
         call_user_func($this->_get);
     }
 
@@ -26,20 +29,26 @@ class APISimple
     {
         if (!$this->_post)
             throw new Exception("Request Method not Allowed", 405);
+
+        $this->checkRequiredParams("post", $_POST);
         call_user_func($this->_post);
     }
     protected function patch()
     {
         if (!$this->_patch)
             throw new Exception("Request Method not Allowed", 405);
-        call_user_func($this->_patch, $this->getParamBody());
+        $params = $this->getParamBody();
+        $this->checkRequiredParams("patch", $params);
+        call_user_func($this->_patch, $params);
     }
     protected function delete()
     {
         if (!$this->_delete)
             throw new Exception("Request Method not Allowed", 405);
 
-        call_user_func($this->_delete);
+        $params = $this->getParamBody();
+        $this->checkRequiredParams("delete", $params);
+        call_user_func($this->_delete, $params);
     }
     protected function options()
     {
@@ -49,35 +58,48 @@ class APISimple
         header("access-control-allow-headers: content-type");
     }
 
-    public function setGet($func)
+    public function setGet($func, $requiredParams = [])
     {
 
         if (!is_callable($func)) {
             throw new Exception("Argument must be an function");
+        }
+        if (count($requiredParams) != 0) {
+            $this->requiredParams["get"] = $requiredParams;
         }
         $this->_get = $func;
     }
-    public function setPost($func)
+    public function setPost($func, $requiredParams = [])
     {
 
         if (!is_callable($func)) {
             throw new Exception("Argument must be an function");
+        }
+        if (count($requiredParams) != 0) {
+            $this->requiredParams["post"] = $requiredParams;
         }
         $this->_post = $func;
     }
-    public function setPatch($func)
+    public function setPatch($func, $requiredParams = [])
     {
 
         if (!is_callable($func)) {
             throw new Exception("Argument must be an function");
         }
+
+        if (count($requiredParams) != 0) {
+            $this->requiredParams["patch"] = $requiredParams;
+        }
         $this->_patch = $func;
     }
-    public function setDelete($func)
+    public function setDelete($func, $requiredParams = [])
     {
 
         if (!is_callable($func)) {
             throw new Exception("Argument must be an function");
+        }
+        if (count($requiredParams) != 0) {
+            $this->requiredParams["delete"] = $requiredParams;
         }
         $this->_delete = $func;
     }
@@ -143,11 +165,19 @@ class APISimple
      */
     protected function parse($string)
     {
-        if (in_array(getallheaders()['Content-Type'], ["application/json", "application/json;charset=utf-8"])) {
-            return json_decode($string, true);
+
+        if (strlen($string) == 0) {
+            throw new Exception("No Arguments were given", 400);
+        }
+        if (key_exists('Content-Type', getallheaders())) {
+
+            if (in_array(getallheaders()['Content-Type'], ["application/json", "application/json;charset=utf-8"])) {
+                return json_decode($string, true);
+            }
         }
         $temp = explode("&", $string);
         $in = [];
+
         foreach ($temp as $value) {
             $item = explode("=", $value);
             $in[$item[0]] = urldecode($item[1]);
@@ -196,5 +226,27 @@ class APISimple
         }
 
         return $res;
+    }
+
+    /**
+     * Required Params provided
+     * @param string $method Request Method
+     * @param array $array $request array
+     * @return bool
+     */
+    protected function checkRequiredParams($method, $array)
+    {
+
+        if (key_exists($method, $this->requiredParams)) {
+
+            foreach ($this->requiredParams[$method] as $value) {
+
+                if (!key_exists($value, $array)) {
+                    throw new Exception("Required Param " . $value . " missing", 400);
+                }
+            }
+        } else {
+            return true;
+        }
     }
 }
