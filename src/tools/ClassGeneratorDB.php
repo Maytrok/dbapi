d4e561503c75ca0830a5e16f773879326a813f30
Maytrok <bluna.el@gmx.net>
2019-10-13 13:43:40 +0200

    Adding ClassGenerator for an whole DB

---
+++ src/tools/ClassGeneratorDB.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace dbapi\tools;
+
+use dbapi\db\Database;
+
+class ClassGeneratorDB
+{
+
+    public function __construct($db, $config = [])
+    {
+        Database::getPDO()->exec("use " . $db);
+        foreach (Database::getPDO()->query("SHOW TABLES") as $value) {
+
+            new ClassGenerator($value['Tables_in_' . $db], $db, $config);
+        }
+    }
+}
